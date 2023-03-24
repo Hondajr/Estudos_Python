7de97311bceb0bf84046e614099d70a67c11d1e1
Hondajr <124377744+Hondajr@users.noreply.github.com>
2023-03-24 17:42:56 -0300

    Add files via upload
    Projeto 1, do livro Big Book of Small Python Projects by Al Sweigart

---
+++ Bagels.py
@@ -0,0 +1,42 @@
+# Pico      Um digito correto, mas posição errada
+# Fermi     Um digito correto, e nao posição correta
+# Bagels    Nenhum digito correto
+# Randomizar um numero de 3 digitos, o usuario deve adivinham em 10 turnos
+
+import random
+
+again = "S"
+while again == "S":
+    numero = list(str(random.randint(1, 999)).zfill(3))
+    print(numero)
+
+    for i in range(0, 11):
+        palpite = list(str(input("Digite um numero: ")).zfill(3))
+        if palpite == numero:
+            print("Parabens, voce adivinhou!!")
+            i = "V"
+            break
+        if palpite[0] not in numero and palpite[1] not in numero and palpite[2] not in numero:
+            print("Bagels")
+        else:
+            if palpite[0] in numero:
+                if palpite[0] == numero[0]:
+                    print("Fermi")
+                else:
+                    print("Pico")
+            if palpite[1] in numero:
+                if palpite[1] == numero[1]:
+                    print("Fermi")
+                else:
+                    print("Pico")
+            if palpite[2] in numero:
+                if palpite[2] == numero[2]:
+                    print("Fermi")
+                else:
+                    print("Pico")
+
+    if i == "V":
+        again = input("Jogar Novamente (S/N)?").upper()
+    else:
+        print("Voce perdeu!!")
+        again = input("Jogar Novamente (S/N)?").upper()
